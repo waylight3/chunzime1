b05409d4ed888b2346d76208f48819ccecc34109
waylight3 <waylight3@naver.com>
2016-12-01 18:55:38 +0900

    test conversation api

--- simple/views.py
+++ simple/views.py
@@ -1,23 +1,37 @@
 from django.shortcuts import render
 from watson_developer_cloud import DocumentConversionV1
+from watson_developer_cloud import ConversationV1
 import json, os
 
-# Create your views here.
 def index(request):
-#    document_conversion = DocumentConversionV1(
-#	    username='9b8f4bc7-9528-4843-b169-f3778bf843b9',
-#	    password='6PsBv4mrDBch',
-#	    version='2015-12-15'
-#	)
-	data = {'test':'fail'}
-#    config = {
-#	'conversion_target': 'ANSWER_UNITS'
-#    }
-#    with open(('sample.pdf'), 'rb') as document:
-#	response = document_conversion.convert_document(document=document, config=config)
-#	data['test'] = response.content.decode()
-	return render(request, 'simple/index.html', data)
+    conversation = ConversationV1(
+            username='e50bf787-668b-4756-824a-6ad1e8d33912',
+            password='68xg1yDhpQOr',
+            version='2016-09-20'
+            )
+    context = {}
+    workspace_id = '95e19486-e402-4c2b-9007-b0b36be7b066'
+    response = conversation.message(
+            workspace_id=workspace_id,
+            message_input={'text': 'Turn on the lights'},
+            context=context
+            )
+    data = {'answer':context}
+    return render(request, 'simple/index.html', data)
 
 def home(request):
-	data = {}
-	return render(request, 'simple/home.html', data)
+    data = {}
+    return render(request, 'simple/home.html', data)
+
+def conversation(request):
+    try:
+        if request.method == 'POST':
+            message = request.POST['message']
+            data = {'answer':'you said "%s".' % message}
+            json_data = json.dumps(data)
+            return HttpResponse(json_data, content_type='application/json')
+    except:
+        data = {'answer':'fail'}
+        json_data = json.dumps(data)
+        return HttpResponse(json_data, content_type='application/json')
+
