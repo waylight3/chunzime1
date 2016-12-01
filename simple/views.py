d0858cab9877285a1619481dbf0cd54e3467abdb
waylight3 <waylight3@naver.com>
2016-12-02 05:35:52 +0900

    connect conversation

--- simple/views.py
+++ simple/views.py
@@ -16,18 +16,18 @@ def conversation(request):
         if request.method == 'GET':
             message = request.GET['message']
             conversation = ConversationV1(
-		            username='e50bf787-668b-4756-824a-6ad1e8d33912',
-		            password='68xg1yDhpQOr',
-		            version='2016-09-20'
-		            )
-		    context = {}
-		    workspace_id = '95e19486-e402-4c2b-9007-b0b36be7b066'
-		    response = conversation.message(
-		            workspace_id=workspace_id,
-		            message_input={'text': message},
-		            context=context
-		            )
-		    data = {'answer':response}
+                    username='e50bf787-668b-4756-824a-6ad1e8d33912',
+                    password='68xg1yDhpQOr',
+                    version='2016-09-20'
+                    )
+            context = {}
+            workspace_id = '95e19486-e402-4c2b-9007-b0b36be7b066'
+            response = conversation.message(
+                    workspace_id=workspace_id,
+                    message_input={'text': message},
+                    context=context
+                    )
+            data = {'answer':response['output']['text']}
             json_data = json.dumps(data)
             return HttpResponse(json_data, content_type='application/json')
     except:
