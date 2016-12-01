9f719d0af7225793fbfc22d07d2c6f55bfd26605
waylight3 <waylight3@naver.com>
2016-12-02 05:26:37 +0900

    add conversation

--- simple/views.py
+++ simple/views.py
@@ -5,19 +5,6 @@ from watson_developer_cloud import ConversationV1
 import json, os
 
 def index(request):
-    conversation = ConversationV1(
-            username='e50bf787-668b-4756-824a-6ad1e8d33912',
-            password='68xg1yDhpQOr',
-            version='2016-09-20'
-            )
-    context = {}
-    workspace_id = '95e19486-e402-4c2b-9007-b0b36be7b066'
-    response = conversation.message(
-            workspace_id=workspace_id,
-            message_input={'text': 'Tell me what is earthquake prevention'},
-            context=context
-            )
-    data = {'answer':response}
     return render(request, 'simple/index.html', data)
 
 def home(request):
@@ -28,7 +15,19 @@ def conversation(request):
     try:
         if request.method == 'GET':
             message = request.GET['message']
-            data = {'answer':'you said "%s".' % message}
+            conversation = ConversationV1(
+		            username='e50bf787-668b-4756-824a-6ad1e8d33912',
+		            password='68xg1yDhpQOr',
+		            version='2016-09-20'
+		            )
+		    context = {}
+		    workspace_id = '95e19486-e402-4c2b-9007-b0b36be7b066'
+		    response = conversation.message(
+		            workspace_id=workspace_id,
+		            message_input={'text': message},
+		            context=context
+		            )
+		    data = {'answer':response}
             json_data = json.dumps(data)
             return HttpResponse(json_data, content_type='application/json')
     except:
