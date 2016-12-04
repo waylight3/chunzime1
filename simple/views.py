1564907e6df6f14d2f950458183192bfb9852df9
waylight3 <waylight3@naver.com>
2016-12-04 15:13:16 +0900

    add comments

--- simple/views.py
+++ simple/views.py
@@ -12,23 +12,38 @@ def home(request):
     data = {}
     return render(request, 'simple/home.html', data)
 
+# ajax로 conversation api를 연동하기 위한 함수
 def conversation(request):
+
+    # get 요청의 경우
     if request.method == 'GET':
+        
+        # api 신임 정보 입력
         conversation = ConversationV1(
                 username='e50bf787-668b-4756-824a-6ad1e8d33912',
                 password='68xg1yDhpQOr',
                 version='2016-09-20'
                 )
+        
+        # 문맥 불러오기
         context = json.loads(request.GET.get('context', '{}'))
+        
+        # 저장소 지정
         workspace_id = '95e19486-e402-4c2b-9007-b0b36be7b066'
+        
+        # api 호출
         response = conversation.message(
                 workspace_id=workspace_id,
                 message_input={'text': request.GET['message']},
                 context=context
                 )
+        
+        # 클라이언트로 응답 반환
         data = {'answer':response['output']['text'], 'context':response['context']}
         json_data = json.dumps(data)
         return HttpResponse(json_data, content_type='application/json')
+
+    # api 응답에 실패한 경우 fail 반환
     data = {'answer':'fail'}
     json_data = json.dumps(data)
     return HttpResponse(json_data, content_type='application/json')
